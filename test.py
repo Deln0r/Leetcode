f6c24cc48a90d4c8057a37ce5d2982d793f63d5a
Ian Chechin <delnor40rus@gmail.com>
2023-08-14 14:36:20 +0300

    123

--- test.py
+++ test.py
@@ -1,18 +1,8 @@
-def count_all(data):
-    d = {}
-    for elem in data:
-        d.update({elem: 1})
-
-    return d
+from collections import defaultdict
 
 
-print(count_all([1, 2, 3, 3, 2, 4]))
-
-
-
-
-def count_all(items):
-    counters = {}
-    for item in items:
-        counters[item] = counters.get(item, 0) + 1
-    return counters
+def collect_indexes(items):
+    result = defaultdict(list)
+    for index, item in enumerate(items):
+        result[item].append(index)
+    return result
