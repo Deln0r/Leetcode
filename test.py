b6fd28da30687a23aff04c87f416212f7975007a
Ian Chechin <delnor40rus@gmail.com>
2023-08-18 19:33:05 +0300

    q

--- test.py
+++ test.py
@@ -1,10 +1,2 @@
-def filter_map(func, items):
-    list = []
-    for item in items:
-        bull, value = func(item)
-        if bull is True:
-            list += value
-        else:
-            continue
-    print(type(list))
-    return list
+lst = ['p', 'y', 't']
+print(lst.clear() is None, bool(lst))
