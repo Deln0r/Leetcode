d9f08ac437afcba19c751962752cfa32dfdbccc5
Ian Chechin <delnor40rus@gmail.com>
2023-08-19 19:29:47 +0300

    1

--- test.py
+++ test.py
@@ -1,3 +1,13 @@
-l = [1, 2, 5, 3, 4]
-l.sort(key=lambda x: -x)
-print(l)
+def quarter_of(month):
+    if month < 4:
+        return 1
+    elif month > 3 and month < 7:
+        return 2
+    elif month > 6 and month < 10:
+        return 3
+    elif month > 9:
+        return 4
+
+
+list = 7
+print(quarter_of(list))
