b6837123708c66d33694be04c95a7341070ff187
Ian Chechin <delnor40rus@gmail.com>
2023-08-22 19:23:36 +0300

    123

--- test.py
+++ test.py
@@ -1,7 +1,17 @@
-value = set(range(1, 101))
-
-
-text1 = 'texts'
-print(value.pop())
-print(value.pop())
-print(value.pop())
+# определение функции декоратора
+def check(input_func):    
+    def output_func(*args):
+        name = args[0]
+        age = args[1]           # получаем значение второго параметра
+        if age < 0: age = 1     # если возраст отрицательный, изменяем его значение на 1
+        input_func(name, age)   # передаем функции значения для параметров
+    return output_func
+ 
+# определение оригинальной функции
+@check
+def print_person(name, age):
+    print(f"Name: {name}  Age: {age}")
+ 
+# вызов оригинальной функции
+print_person("Tom", 38)
+print_person("Bob", -5)
