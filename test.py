a29c8b31460635900ab11d93e98480c15e8052b4
Ian Chechin <delnor40rus@gmail.com>
2023-08-23 21:35:50 +0300

    8

--- test.py
+++ test.py
@@ -1,17 +1,10 @@
-# определение функции декоратора
-def check(input_func):    
-    def output_func(*args):
-        name = args[0]
-        age = args[1]           # получаем значение второго параметра
-        if age < 0: age = 1     # если возраст отрицательный, изменяем его значение на 1
-        input_func(name, age)   # передаем функции значения для параметров
-    return output_func
- 
-# определение оригинальной функции
-@check
-def print_person(name, age):
-    print(f"Name: {name}  Age: {age}")
- 
-# вызов оригинальной функции
-print_person("Tom", 38)
-print_person("Bob", -5)
+def removeDuplicates(nums):
+        len1 = len(nums)
+        new_set = set(nums)
+        k = len(new_set)
+        new_list = list(new_set)
+        new_list.append(_)
+        return k, new_list
+
+nums = [0,0,1,1,1,2,2,3,3,4]
+print(removeDuplicates(nums))
