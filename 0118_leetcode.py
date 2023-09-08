480bbbd26f54ea4fbc333c6dfde5203c84b56d2c
Ian Chechin <delnor40rus@gmail.com>
2023-09-08 19:45:39 +0300

    08.09

--- 0118_leetcode.py
+++ 0118_leetcode.py
@@ -5,8 +5,8 @@ class Solution:
             return dp[:numRows]
         for _ in range(numRows - 2):
             next_row = [1]
-            for i in range(1, len(dp[-1])):
-                next_row.append(dp[-1][i] + dp[-1][i-1])
+            for i in range(1, len(dp[- 1])):
+                next_row.append(dp[- 1][i] + dp[- 1][i - 1])
             next_row += [1]
             dp.append(next_row)
         return dp
