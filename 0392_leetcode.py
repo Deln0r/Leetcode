23533897bfba30ee90d7077d1131eedb06c9292c
Ian Chechin <Delnor40rus@gmail.com>
2023-09-22 11:28:47 +0300

    22.09

--- 0392_leetcode.py
+++ 0392_leetcode.py
@@ -5,3 +5,9 @@ class Solution:
             if stack and stack[-1] == c:
                 stack.pop()
         return len(stack) == 0
+    
+class Solution:
+    def isSubsequence(self, s: str, t: str) -> bool:
+        x=iter(t)
+        return all(ch in x for ch in s)
+
