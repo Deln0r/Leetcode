5764ad57533887e710936c171233d0237b9a8f92
Ian Chechin <Delnor40rus@gmail.com>
2023-09-24 18:36:26 +0300

    24.09

--- tests.py
+++ tests.py
@@ -1,3 +1,16 @@
-new_list = [1,2]
-new_list.extend(new_list)
-print(new_list)
+employee_list = [
+   {"id": 12345, "name": "John", "department": "Kitchen"},
+   {"id": 12456, "name": "Paul", "department": "House Floor"},
+   {"id": 12478, "name": "Sarah", "department": "Management"},
+   {"id": 12434, "name": "Lisa", "department": "Cold Storage"},
+   {"id": 12483, "name": "Ryan", "department": "Inventory Mgmt"},
+   {"id": 12419, "name": "Gill", "department": "Cashier"}
+]
+
+
+def map_id_to_initial(employee_list):
+    
+    dict = {i["name"][0]:i['id'] for i in employee_list}
+    return dict
+   
+print(map_id_to_initial(employee_list))
