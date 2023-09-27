0670f7ecd38e081ddf669643bb3cf920b955b208
Ian Chechin <Delnor40rus@gmail.com>
2023-09-27 17:39:45 +0300

    27.09

--- tests.py
+++ tests.py
@@ -1,17 +1,10 @@
-class A:
-    def c(self):
-        return "Function inside A"
-
-class B:
-    def c(self):
-        return "Function inside B"
-
-class C(A, B):
-    def c(self):
-        return "Function inside C"
-
-class D(A, C):
-    pass
-
-d = D()
-print(d.c)
+def d():
+    color = 'green'
+    def e():
+        nonlocal color
+        color = 'yellow'
+    e()
+    print('color:' + color)
+    color = 'red'
+color = "blue"
+d()
