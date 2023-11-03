f4e6e7d7eee15b173addd51b00241bdb377ab90f
Ian Chechin <Delnor40rus@gmail.com>
2023-11-03 19:20:41 +0300

    03.11

--- tests.py
+++ tests.py
@@ -1,3 +1,26 @@
-num = [1,2,3,4,5]
-num.remove(2)
-print(num)
+import re
+
+def multi_vowel_words(text):
+    pattern = r'\b\w*[aeiouAEIOU]{3,}\w*\b'
+    result = re.findall(pattern, text)
+    return result
+
+# Example usage:
+text = "This is a simple example with multiple vowels like eiwu."
+result = multi_vowel_words(text)
+print(result)
+# Should print: ['aaaeeeiouuu']
+print(multi_vowel_words("Life is beautiful")) 
+# ['beautiful']
+
+print(multi_vowel_words("Obviously, the queen is courageous and gracious.")) 
+# ['Obviously', 'queen', 'courageous', 'gracious']
+
+print(multi_vowel_words("The rambunctious children had to sit quietly and await their delicious dinner.")) 
+# ['rambunctious', 'quietly', 'delicious']
+
+print(multi_vowel_words("The order of a data queue is First In First Out (FIFO)")) 
+# ['queue']
+
+print(multi_vowel_words("Hello world!")) 
+# []
