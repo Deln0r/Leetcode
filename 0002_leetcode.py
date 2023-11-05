401768f8cc9a25ec03dfcffef7c8658de7c44407
Ian Chechin <Delnor40rus@gmail.com>
2023-11-05 13:20:18 +0300

    05.11

---
+++ 0002_leetcode.py
@@ -0,0 +1,22 @@
+        res=l1
+        last=0
+        carry=ListNode()
+        while l1 and l2:
+            temp=l1.val+l2.val+carry.val
+            carry.val=temp//10
+            l1.val=temp%10
+            last=l1
+            l1=l1.next
+            l2=l2.next
+        if l2: #if l2 has more nodes than l1
+            last.next=l2
+            l1=l2
+        while l1 and carry.val:
+            temp=l1.val+carry.val
+            carry.val=temp//10
+            l1.val=temp%10
+            last=l1
+            l1=l1.next
+        if carry.val:
+            last.next=carry
+        return res
