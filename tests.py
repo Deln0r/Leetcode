499ea45111f6b9bbb05622737d5e24720a47d5f8
Ian Chechin <Delnor40rus@gmail.com>
2023-11-28 18:39:45 +0300

    28.11

--- tests.py
+++ tests.py
@@ -2,11 +2,14 @@ def get_sum(a,b):
     result = 0
     if a > b:
         a, b = b, a
+        
     elif a == b:
         return b
+    
     while a <= b:
         result += a
         a += 1
+        
     return result
 
 print(get_sum(5,0))
