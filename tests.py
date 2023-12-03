eb503e31c0fb0272dd0a4e58c30168405d6de867
Ian Chechin <Delnor40rus@gmail.com>
2023-12-03 19:34:29 +0300

    03.12

--- tests.py
+++ tests.py
@@ -1,9 +1,12 @@
-L = [1,2,3,4,5,6,7,8,9,0]
-f1 = [x+1 for x in L]
-f2 = (x+1 for x in L)
-print(f1)
-print(f2)
-print(f2.__next__())
-print(next(f2))
-print(iter(f2))
-        
+import itertools, operator
+
+list1 = {1, 3, 5}
+res = itertools.accumulate(list1)
+print('default:')
+for x in res:
+    print(x)
+    
+res = itertools.accumulate(list1, operator.mul)
+print('multiply:')
+for x in res:
+    print(x)
