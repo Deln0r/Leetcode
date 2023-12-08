46d2ce1c49b123d46f00d29b868892558c217a10
Ian Chechin <Delnor40rus@gmail.com>
2023-12-08 14:45:31 +0300

    08.12

--- study.py
+++ study.py
@@ -1,22 +1,23 @@
-from threading import current_thread, Thread
-from time import sleep
+from threading import Thread, Lock
 
-def daemon_func():
-    print(current_thread().daemon)
-    sleep(3)
-    print('{}: Hello from daemon'.format(current_thread().name))
+def inc(lock):
+    global x
+    for i in range(1000000):
+        lock.acquire()
+        x += 1
+        lock.release()
+        
+mylock = Lock()
 
-def nondaemon_func():
-    print(current_thread().daemon)
-    sleep(1)
-    print('{}: Hello from non-daemon'.format(current_thread().name))
-    
-t1 = Thread(target=daemon_func, name='Daemon thread', daemon=True)
-t2 = Thread(target=nondaemon_func, name='Non-Daemon thread')
+x = 0
+
+t1 = Thread(target=inc, args=(mylock,), name="Th 1")
+t2 = Thread(target=inc, args=(mylock,), name="Th 2")
 
 t1.start()
 t2.start()
 
 t1.join()
+t2.join()
 
-print('Exiting the main program')
+print('Final value of x:', x)
