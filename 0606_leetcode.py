2592b4d216e96f98b68c6f31c222488d9ffc624a
Ian Chechin <Delnor40rus@gmail.com>
2023-12-08 11:22:49 +0300

    08.12

--- 0606_leetcode.py
+++ 0606_leetcode.py
@@ -11,3 +11,28 @@ class Solution:
         elif left and not right:
           s += ["(", left, ")"]
         return "".join(s)
+
+
+# class Solution(object):
+#     def tree2str(self, t):
+#         res = []
+#         self.dfs(t, res)
+#         return ''.join(res)
+
+#     def dfs(self, t, res):
+#         if t is None:
+#             return
+
+#         res.append(str(t.val))
+
+#         if t.left is None and t.right is None:
+#             return
+
+#         res.append('(')
+#         self.dfs(t.left, res)
+#         res.append(')')
+
+#         if t.right is not None:
+#             res.append('(')
+#             self.dfs(t.right, res)
+#             res.append(')')
