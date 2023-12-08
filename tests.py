60a143cbb911f93476c950428411cd915da2c423
Ian Chechin <Delnor40rus@gmail.com>
2023-12-08 14:09:20 +0300

    08.12

--- tests.py
+++ tests.py
@@ -1,13 +1,22 @@
-import itertools, operator
+from threading import current_thread, Thread
+from time import sleep
 
-list1 = {1, 3, 5}
-res = itertools.accumulate(list1)
-print('default:')
-for x in res:
-    print(x)
+def print_hello():
+    sleep(3)
+    print('{}: Hello'.format(current_thread().name))
 
+def print_message(msg):
+    sleep(1.5)
+    print('{}:{}'.format(current_thread().name, msg))
 
-res = itertools.accumulate(list1, operator.mul)
-print('multiply:')
-for x in res:
-    print(x)
+t1 = Thread(target=print_hello, name='Th 1')
+t2 = Thread(target=print_hello, name='Th 2')
+t3 = Thread(target=print_message, args=['Good morning'], name='Th 3')
+
+t1.start()
+t2.start()
+t3.start()
+
+t1.join()
+t2.join()
+t3.join()
