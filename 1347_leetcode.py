c2cb937d236b905ca7809a9ddc00a096e8806d16
Ian Chechin <Delnor40rus@gmail.com>
2024-01-13 13:46:46 +0300

    13.01

--- 1347_leetcode.py
+++ 1347_leetcode.py
@@ -3,3 +3,15 @@ class Solution:
         return sum((Counter(t)-Counter(s)).values())
 
 
+class Solution:
+    def minSteps(self, s: str, t: str) -> int:
+        count_s, count_t = Counter(s), Counter(t)
+        res = 0
+        
+        for  k, v in count_s.items():
+            num = count_t.get(k)
+            
+            if num is None: res += v
+            elif num < v:   res += (v - num)
+
+        return res
