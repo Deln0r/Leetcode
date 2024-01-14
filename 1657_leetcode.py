55bdebcb95c664dadc33c418a93a7a9feb2cdf88
Ian Chechin <Delnor40rus@gmail.com>
2024-01-14 15:50:25 +0300

    14.01

---
+++ 1657_leetcode.py
@@ -0,0 +1,12 @@
+class Solution:
+    def closeStrings(self, word1: str, word2: str) -> bool:
+        
+        if len(word1)!=len(word2):
+            return False
+
+        counter1=Counter(word1)
+        counter2=Counter(word2)
+        num= set(word1)^set(word2)
+        if sorted(counter1.values())==sorted(counter2.values()) and len(num)==0:
+            return True
+        return False
