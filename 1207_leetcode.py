38f61f16bb518c714b18bb71d7126f978429d0a3
Ian Chechin <Delnor40rus@gmail.com>
2024-01-17 13:01:26 +0300

    17.01

---
+++ 1207_leetcode.py
@@ -0,0 +1,17 @@
+class Solution:
+    def uniqueOccurrences(self, arr: List[int]) -> bool:
+        has = {}
+        for i in arr:
+            if i in has:
+                has[i]+=1
+            else:
+                has[i]=1
+
+        freq = set()
+        
+        for val in has:
+            if has[val] in freq:
+                return False
+            freq.add(has[val])
+        
+        return True
