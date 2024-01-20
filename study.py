79ba1b5a1f67b7493e6f29664c840041aa962355
Ian Chechin <Delnor40rus@gmail.com>
2024-01-20 13:03:19 +0300

    20.01

--- study.py
+++ study.py
@@ -11,19 +11,13 @@ mylock = Lock()
 
 x = 0
 
-
-
-
 t1 = Thread(target=inc, args=(mylock,), name="Th 1")
 t2 = Thread(target=inc, args=(mylock,), name="Th 2")
-
 
 t1.start()
 t2.start()
 
-
 t1.join()
 t2.join()
-
 
 print('Final value of x:', x)
