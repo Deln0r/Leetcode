4a7ef4d1ad133d1b1f702a6f83e75e207523ad48
Ian Chechin <Delnor40rus@gmail.com>
2024-02-02 13:23:57 +0300

    02.02

--- 1291_leetcode.py
+++ 1291_leetcode.py
@@ -9,3 +9,16 @@ class Solution:
                 next_digit += 1
                 current = current * 10 + next_digit
         return sorted(result)
+
+
+class Solution:
+    def sequentialDigits(self, low: int, high: int) -> List[int]:
+        s='123456789'
+        ans=[]
+        for i in range(len(s)):
+            for j in range(i+1,len(s)):
+                st=int(s[i:j+1])
+                if(st>=low and st<=high):
+                    ans.append(st)
+        ans.sort()            
+        return ans  
