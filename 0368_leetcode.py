da2a6d83c6eca59b3168969b3e1ea562501c3e7d
Ian Chechin <Delnor40rus@gmail.com>
2024-02-09 13:55:40 +0300

    09.02

---
+++ 0368_leetcode.py
@@ -0,0 +1,20 @@
+class Solution:
+    def largestDivisibleSubset(self, nums):
+        
+     
+        n= len(nums)
+        
+        if n==0:return []
+        
+        nums.sort()
+        
+        dp=[ [i] for i in nums]
+        
+        for i in range(n):
+            for j in range(i):
+                
+                if nums[i]%nums[j]==0 and len(dp[j])+1 > len(dp[i]):
+                    
+                    dp[i] = dp[j]+[nums[i]]
+        
+        return max(dp, key=len)
