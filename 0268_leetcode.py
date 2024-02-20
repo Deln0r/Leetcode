0ca245ad9cac9ba3575acf55ec4e3a068d1d101d
Ian Chechin <Delnor40rus@gmail.com>
2024-02-20 13:19:08 +0300

    20.02

--- 0268_leetcode.py
+++ 0268_leetcode.py
@@ -1,3 +1,8 @@
 class Solution:
     def missingNumber(self, nums: List[int]) -> int:
         return (len(nums) * (len(nums) + 1))//2 - sum(nums)
+
+
+class Solution:
+    def missingNumber(self, nums: List[int]) -> int:
+        return next(i for i in range(len(nums) + 1) if i not in nums)
