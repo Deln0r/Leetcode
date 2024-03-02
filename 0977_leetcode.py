a62f009c6a092636c15516b72527fe877f0a9a33
Ian Chechin <Delnor40rus@gmail.com>
2024-03-02 11:46:12 +0300

    02.03

---
+++ 0977_leetcode.py
@@ -0,0 +1,21 @@
+class Solution:
+    def sortedSquares(self, nums: List[int]) -> List[int]:
+        
+        i = 0
+        n = len(nums)
+        j = n - 1
+        
+        new = [0] * n
+        k = n - 1
+        
+        while i <= j:
+            if abs(nums[i]) < abs(nums[j]):
+                new[k] = nums[j] ** 2
+                j -= 1
+                
+            else:
+                new[k] = nums[i] ** 2
+                i += 1
+            k -= 1
+            
+        return new
