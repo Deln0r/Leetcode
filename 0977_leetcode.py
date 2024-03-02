cd251b4c2554d98294bd0552212e24d2f25917a6
Ian Chechin <Delnor40rus@gmail.com>
2024-03-02 11:46:27 +0300

    02.02

--- 0977_leetcode.py
+++ 0977_leetcode.py
@@ -19,3 +19,12 @@ class Solution:
             k -= 1
             
         return new
+    
+    
+    
+class Solution:
+    def sortedSquares(self, nums: List[int]) -> List[int]:
+        for i in range(len(nums)):
+            nums[i] = (nums[i]**2)
+        nums.sort()
+        return nums
