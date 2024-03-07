2dc3e6615870a6eb821a76fe7abab27a772a14d9
Ian Chechin <Delnor40rus@gmail.com>
2024-03-07 12:52:21 +0300

    07.03

--- 0876_leetcode.py
+++ 0876_leetcode.py
@@ -6,3 +6,18 @@ class Solution:
             slow = slow.next
             fast = fast.next.next
         return slow
+
+
+class Solution:
+    def middleNode(self, head: ListNode) -> ListNode:
+        if not head:
+            return head
+        
+        fast = head
+        slow = head
+
+        while fast and fast.next:
+            slow = slow.next
+            fast = fast.next.next
+
+        return slow
