4f4ae7ed44b6af89fd7f78ac2ac4ac9e4bc85242
Ian Chechin <Delnor40rus@gmail.com>
2024-03-24 13:56:36 +0300

    24.03

--- 0287_leetcode.py
+++ 0287_leetcode.py
@@ -5,3 +5,10 @@ class Solution:
             if nums[a-1] < 0: return a
             else:
                 nums[a-1]*=-1
+                
+
+class Solution:
+    def findDuplicate(self, nums: List[int]) -> int:
+        nums=sorted(nums)
+        for i in range(len(nums)-1):
+            if nums[i]==nums[i+1]:   return nums[i] 
