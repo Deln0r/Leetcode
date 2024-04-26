4ed8f5fe07ea00cee3868869a989b0a5dd2eb8d3
Ian Chechin <Delnor40rus@gmail.com>
2024-04-26 10:40:25 +0300

    26.04

--- algo_study.py
+++ algo_study.py
@@ -178,10 +178,15 @@
 # contains_all = set(some_list).issubset(set(first_tuple))
 # print(contains_all)
 
-import re
-email_regex = r'\b[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Z|a-z]{2,}\b'
-def is_valid_email(email):
-    if re.match(email_regex, email):
-        return True
-    else:
-        return False
+# import re
+# email_regex = r'\b[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Z|a-z]{2,}\b'
+# def is_valid_email(email):
+#     if re.match(email_regex, email):
+#         return True
+#     else:
+#         return False
+
+x = True
+y = False
+
+print(y and x or x)
