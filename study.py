acd0b8d18a2524f6fca0dabaa0f88e99f2e0628b
Ian Chechin <Delnor40rus@gmail.com>
2024-05-02 19:52:49 +0300

    02.05

--- study.py
+++ study.py
@@ -1,23 +1,24 @@
-from threading import Thread, Lock
+# from threading import Thread, Lock
 
-def inc(lock):
-    global x
-    for i in range(1000000):
-        lock.acquire()
-        x += 1
-        lock.release()
+# def inc(lock):
+#     global x
+#     for i in range(1000000):
+#         lock.acquire()
+#         x += 1
+#         lock.release()
         
-mylock = Lock()
+# mylock = Lock()
 
-x = 0
+# x = 0
 
-t1 = Thread(target=inc, args=(mylock,), name="Th 1")
-t2 = Thread(target=inc, args=(mylock,), name="Th 2")
+# t1 = Thread(target=inc, args=(mylock,), name="Th 1")
+# t2 = Thread(target=inc, args=(mylock,), name="Th 2")
 
-t1.start()
-t2.start()
+# t1.start()
+# t2.start()
 
-t1.join()
-t2.join()
+# t1.join()
+# t2.join()
 
-print('Final value of x:', x)
+# print('Final value of x:', x)
+
