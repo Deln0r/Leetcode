6e4e6a3f0d457b38fdfae913534e20299fcd203c
Ian Chechin <Delnor40rus@gmail.com>
2024-06-07 11:55:23 +0300

    07.06

---
+++ 0648_leetcode.go
@@ -0,0 +1,19 @@
+func replaceWords(dictionary []string, sentence string) string {
+	words := strings.Fields(sentence)
+
+	rootMap := make(map[string]bool)
+	for _, root := range dictionary {
+		rootMap[root] = true
+	}
+
+	for i, word := range words {
+		for j := 1; j <= len(word); j++ {
+			if rootMap[word[:j]] {
+				words[i] = word[:j]
+				break
+			}
+		}
+	}
+
+
+	return strings.Join(words, " ")
