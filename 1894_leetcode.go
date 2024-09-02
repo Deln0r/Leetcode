2e008d20f7e4930a7e760dd8d7f76b8eddf03874
Ian Chechin <Delnor40rus@gmail.com>
2024-09-02 10:29:46 +0300

    02.09

---
+++ 1894_leetcode.go
@@ -0,0 +1,18 @@
+
+func chalkReplacer(chalk []int, k int) int {
+	l := len(chalk)
+	for i := 1; i < l; i++ {
+		chalk[i] += chalk[i-1]
+	}
+	k %= chalk[l-1]
+	idx := sort.Search(l, func(i int) bool {
+		return chalk[i] >= k
+	})
+	if idx == l {
+		return 0
+	}
+	if chalk[idx] == k {
+		return (idx + 1) % l
+	}
+	return idx
+}
