30794daada03e51b87f63325a3f4ae0faeaf1489
Ian Chechin <Delnor40rus@gmail.com>
2024-09-17 19:39:56 +0300

    17.09

--- 0605_leetcode.py
+++ 0605_leetcode.py
@@ -6,3 +6,15 @@ class Solution:
                 flowerbed[i] = 1
                 n -= 1
         return n <= 0
+
+# 17.09.2024
+class Solution:
+    def canPlaceFlowers(self, flowerbed: List[int], n: int) -> bool:
+        flowerbed = [0] + flowerbed + [0]
+        counter = 0
+        for i in range(1, len(flowerbed)-1):
+            if flowerbed[i-1] + flowerbed[i] + flowerbed[i+1] == 0:
+                flowerbed[i] = 1
+                counter += 1
+
+        return counter >= n
