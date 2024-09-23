b700725ca9471e14c2f01ee7a87513fe344821d2
Ian Chechin <Delnor40rus@gmail.com>
2024-09-23 14:34:43 +0300

    23.09

--- 2707_leetcode.py
+++ 2707_leetcode.py
@@ -13,3 +13,52 @@ class Solution:
                     dp[i] = min(dp[i], dp[i-l])
                     
         return dp[-1]
+
+
+class Solution:
+    def minExtraChar(self, s: str, dictionary: List[str]) -> int:
+
+        # Build a trie with reversed words in dictionary
+        trie = {}
+        for w in dictionary:
+            w = reversed(w)
+            cur = trie
+            for c in w:
+                if c not in cur:
+                    cur[c] = {}
+                cur = cur[c]
+            cur['match'] = True
+
+        def getDictWord(i, trie, s):
+            '''Retrieve the lookback indices with revserd trie and index.'''
+            cur = trie
+            res = []
+            for j in range(i+1)[::-1]:
+                if s[j] in cur:
+                    cur = cur[s[j]]
+                    if 'match' in cur:
+                        res += [j]
+                else:
+                    break
+            return res
+
+
+        n = len(s)
+        dp = []
+        for i in range(n):
+            # - Fetch lookback indices
+            lookback = getDictWord(i, trie, s)
+            # - Initial cost
+            if dp:
+                v = dp[-1] + 1
+            else:
+                v = 1
+            # - Optimized the cost with lookback indices
+            if lookback:
+                for j in lookback:
+                    if j == 0:
+                        v = 0
+                        break
+                    v = min(v, dp[j-1])
+            dp += [v]
+        return dp[-1]
