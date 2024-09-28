cb93e87393855247470ce0fab185c7cdf19b9dee
Ian Chechin <Delnor40rus@gmail.com>
2024-09-28 14:50:07 +0300

    28.09

--- 1313_leetcode.py
+++ 1313_leetcode.py
@@ -4,3 +4,11 @@ class Solution:
         for i in range(0 ,len(nums), 2):
             ans += [nums[i+1]] * nums[i]
         return ans
+    
+
+class Solution:
+    def decompressRLElist(self, N: List[int]) -> List[int]:
+        L, A = len(N), []
+        for i in range(0,L,2):
+            A.extend(N[i]*[N[i+1]])
+        return A
