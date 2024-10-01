01f4ec943a317e6813d2749ff257b18f044db65f
Ian Chechin <Delnor40rus@gmail.com>
2024-10-01 18:37:06 +0300

    01.10

--- 0896_leetcode.py
+++ 0896_leetcode.py
@@ -10,3 +10,17 @@ class Solution:
                 increasing_index = False
 
         return increasing_index or decreasing_index
+    
+    
+
+class Solution:
+    def isMonotonic(self, nums:list[int]) -> bool:
+        inc = True
+        dec = True
+
+        for i in range(1, len(nums)):
+            if nums[i] > nums[i-1]:
+                dec = False
+            elif nums[i] < nums[i-1]:
+                inc = False
+        return inc or dec
