5de23297dba52dc2cd1cca89465de38e54c474f4
Ian Chechin <Delnor40rus@gmail.com>
2024-10-04 13:13:21 +0300

    04.10

--- 2706_leetcode.py
+++ 2706_leetcode.py
@@ -12,3 +12,17 @@ class Solution:
             return money
         else:
             return money - (min1 + min2)
+
+
+class Solution:
+    def buyChoco(self, prices: List[int], money: int) -> int:
+        min1 = float('inf')
+        min2 = float('inf')
+
+        for p in prices:
+            if p < min1:
+                min2, min1 = min1, p
+            elif p < min2:
+                min2 = p
+        ans = money - min1 - min2
+        return ans if ans>=0 else money
