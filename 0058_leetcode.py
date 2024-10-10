64d31a30157d7fa594b04b08a1ba1e42e188e0d9
Ian Chechin <Delnor40rus@gmail.com>
2024-10-10 22:11:23 +0300

    10.10

--- 0058_leetcode.py
+++ 0058_leetcode.py
@@ -6,3 +6,18 @@ class Solution:
             i-=1
 
         return len(s)-i-1
+    
+class Solution:
+    def lengthOfLastWord(self, s: str) -> int:
+        ans = 0
+        for i in range(len(s)-1, -1, -1):
+            if s[i] != ' ':
+                ans += 1
+            elif s[i] == ' ' and ans != 0:
+                return ans
+        return ans
+    
+
+class Solution:
+    def lengthOfLastWord(self, s: str) -> int:
+        return len(s.split()[-1])
