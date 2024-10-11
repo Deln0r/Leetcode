9fa2e703019a5cb2a4a17adba8e67709af0f8544
Ian Chechin <Delnor40rus@gmail.com>
2024-10-11 18:27:30 +0300

    11.10

---
+++ 2446_leetcode.py
@@ -0,0 +1,10 @@
+class Solution:
+    def maximumValue(self, strs: List[str]) -> int:
+        ans = 0
+
+        for s in strs:
+            if s.isdigit():
+                ans = max(ans, int(s))
+            else:
+                ans = max(ans, len(s))
+        return ans
