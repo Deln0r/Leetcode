49cd80d6b6474ea67edcb2245dc274e4fb313489
Ian Chechin <Delnor40rus@gmail.com>
2024-10-12 17:29:49 +0300

    12.10

--- 0066_leetcode.py
+++ 0066_leetcode.py
@@ -7,3 +7,14 @@ class Solution:
                 digits[i] += 1
                 return digits
         return [1] + digits
+
+class Solution:
+    def plusOne(self, digits: List[int]) -> List[int]:
+        carry = 1
+        for i in range(len(digits)-1, -1, -1):
+            cur = digits[i] + carry
+            digits[i] = cur % 10
+            carry = cur // 10
+        if carry:
+            return [carry]+digits
+        return digits
