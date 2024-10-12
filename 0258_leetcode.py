776d99dede40b9b31547ae0b26b144f608d10e95
Ian Chechin <Delnor40rus@gmail.com>
2024-10-12 17:29:52 +0300

    12.10

--- 0258_leetcode.py
+++ 0258_leetcode.py
@@ -8,3 +8,21 @@ class Solution:
                 new_num += d
             num = new_num
         return num
+
+class Solution:
+    def addDigits(self, num: int) -> int:
+        l = sum([int(n) for n in str(num)])
+        while l > 9:
+            l = sum([int(n) for n in str(l)])
+        return l
+
+class Solution:
+    def addDigits(self, num: int) -> int:
+        while num > 9:
+            new_num = 0
+            temp = num
+            while temp:
+                temp, d = divmod(temp, 10)
+                new_num += d
+            num = new_num
+        return num
