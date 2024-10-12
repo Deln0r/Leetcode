6cf19597fdb147e1b30dfa0d521a76fa647de459
Ian Chechin <Delnor40rus@gmail.com>
2024-10-12 17:29:55 +0300

    12.10

--- 1281_leetcode.py
+++ 1281_leetcode.py
@@ -3,3 +3,17 @@ class Solution:
     def subtractProductAndSum(self, n: int) -> int:
         arr = [int(i) for i in str(n)]
         return math.prod(arr) - sum(arr)
+
+
+class Solution:
+    def subtractProductAndSum(self, n: int) -> int:
+        summ = 0
+        prod = 1
+
+        while n:
+            d = n % 10
+            n = n // 10
+            summ += d
+            prod *= d
+        
+        return prod - summ
