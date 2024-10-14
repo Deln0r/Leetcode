c25d846565cac7b83212a14b5154a814d06f3c43
Ian Chechin <Delnor40rus@gmail.com>
2024-10-14 17:53:27 +0300

    14.10

--- 0217_leetcode.py
+++ 0217_leetcode.py
@@ -5,3 +5,13 @@ def containsDuplicate(nums):
 nums = [1, 2, 3, 4]
 # nums = [1, 2, 3, 1]
 print(containsDuplicate(nums))
+
+
+class Solution:
+    def containsDuplicate(self, nums: List[int]) -> bool:
+        seen = set()
+        for num in nums:
+            if num in seen:
+                return True
+            seen.add(num)
+        return False
