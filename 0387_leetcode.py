1402cb1e97abaa27d07bcae90ff20969b95c5026
Ian Chechin <Delnor40rus@gmail.com>
2024-10-15 20:45:13 +0300

    15.10

--- 0387_leetcode.py
+++ 0387_leetcode.py
@@ -2,3 +2,18 @@ class Solution:
     def firstUniqChar(self, s: str) -> int:
         c = Counter(s)
         return min([i for i in range(len(s)) if c[s[i]] == 1], default = -1)
+    
+
+class Solution:
+    def firstUniqChar(self, s: str) -> int:
+        my_dict = {}
+        for i in s:
+            if i in my_dict:
+                my_dict[i] += 1
+            else:
+                my_dict[i] = 1
+        
+        for i in range(len(s)):
+            if my_dict[s[i]] == 1:
+                return i
+        return -1
