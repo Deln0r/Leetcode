9446815cb73ab9f829a05b74e1bd9fda3f579fd2
Ian Chechin <Delnor40rus@gmail.com>
2024-10-15 20:45:16 +0300

    15.10

--- 1160_leetcode.py
+++ 1160_leetcode.py
@@ -8,3 +8,19 @@ class Solution:
             else:
                 length.append(len(i))
         return sum(length)
+
+
+class Solution:
+    def countCharacters(self, words: List[str], chars: str) -> int:
+        ans = 0
+        ch = Counter(chars)
+
+        for word in words:
+            w = Counter(word)
+            good = True
+            for c in w:
+                if w[c] > ch[c]:
+                    good = False
+            if good:
+                ans += len(word)
+        return ans
