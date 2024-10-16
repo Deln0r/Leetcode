de1752e55ed2493ac882640133af90e1a954b152
Ian Chechin <Delnor40rus@gmail.com>
2024-10-16 21:00:11 +0300

    16.10

--- 1512_leetcode.py
+++ 1512_leetcode.py
@@ -6,3 +6,16 @@ class Solution:
                 if nums[i]==nums[j]:
                     c+=1
         return c
+    
+
+class Solution:
+    def numIdenticalPairs(self, nums: List[int]) -> int:
+        ans = 0
+        d = {}
+        for num in nums:
+            if num in d:
+                ans += d[num]
+                d[num] += 1
+            else:
+                d[num] = 1
+        return ans
