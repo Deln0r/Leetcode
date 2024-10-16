7787c6afc457b60a6481ef7d916174ebd51762e0
Ian Chechin <Delnor40rus@gmail.com>
2024-10-16 21:00:01 +0300

    16.10

--- 0001_leetcode.py
+++ 0001_leetcode.py
@@ -1,8 +1,18 @@
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
+        d = {}
+        for i, n in enumerate(nums):
+            d[n]=i
+
+        for i, n in enumerate(nums):
+            pair = target - n
+            if pair in d and i != d[pair]:
+                return i, d[pair]
+
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
         dict={}
         for i,n in enumerate(nums):
-            if n in dict:
-                return dict[n],i
-            else:
-                dict[target-n]=i
+            if target-n in dict:
+                return dict[target-n],i
+            dict[n]=i
