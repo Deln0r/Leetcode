c5922efa015b655f8a4e5cc30c061246028298e5
Ian Chechin <Delnor40rus@gmail.com>
2024-11-08 21:35:02 +0300

    08.11

--- 0125_leetcode.py
+++ 0125_leetcode.py
@@ -22,3 +22,22 @@ class Solution:
             left += 1
             r -= 1
         return True
+
+
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        new_s = []
+
+        for ch in s:
+            if ch.isalnum():
+                new_s.append(ch.lower())
+
+        l, r = 0, len(new_s)-1
+
+        while l < r:
+            if new_s[l] == new_s[r]:
+                l += 1
+                r -= 1
+            else:
+                return False
+        return True
