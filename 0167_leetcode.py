db431871aa510c6b64a9647593dc41393e98eef8
Ian Chechin <Delnor40rus@gmail.com>
2024-11-08 21:35:04 +0300

    08.11

--- 0167_leetcode.py
+++ 0167_leetcode.py
@@ -10,3 +10,14 @@ class Solution:
                 r -= 1
             else:
                 l += 1
+
+class Solution:
+    def twoSum(self, numbers: List[int], target: int) -> List[int]:
+        l, r = 0, len(numbers)-1
+        while l < r:
+            if numbers[l] + numbers[r] > target:
+                r-=1
+            elif numbers[l] + numbers[r] < target:
+                l+=1
+            else:
+                return [l+1, r+1]
