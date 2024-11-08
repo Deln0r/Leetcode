9f35bb4301ebe2eefc898f78c9efea01bfce4e67
Ian Chechin <Delnor40rus@gmail.com>
2024-11-08 21:35:06 +0300

    08.11

--- 0283_leetcode.py
+++ 0283_leetcode.py
@@ -5,3 +5,17 @@ class Solution:
             if nums[i] != 0:
                 nums[i], nums[arr] = nums[arr], nums[i]
                 arr += 1
+
+class Solution:
+    def moveZeroes(self, nums: List[int]) -> None:
+        """
+        Do not return anything, modify nums in-place instead.
+        """
+        l, r = 0, 0
+        while r < len(nums):
+            if nums[r] != 0:
+                nums[l], nums[r] = nums[r], nums[l]
+                l+=1
+                r+=1
+            else:
+                r+=1
