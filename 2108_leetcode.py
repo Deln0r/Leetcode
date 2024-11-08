ba4ef9fed566c45f3041187b0d3b141043bd8ef8
Ian Chechin <Delnor40rus@gmail.com>
2024-11-08 21:35:18 +0300

    08.11

--- 2108_leetcode.py
+++ 2108_leetcode.py
@@ -4,3 +4,19 @@ class Solution:
             if i==i[::-1]:
                  return i
         return ""
+    
+
+class Solution:
+    def firstPalindrome(self, words: List[str]) -> str:
+        for i in words:
+            l, r = 0, len(i)-1
+            flag = True
+            while l<r:
+                if i[l] != i[r]:
+                    flag = False
+                    break
+                l+=1
+                r-=1
+            if flag:
+                return i
+        return ""
