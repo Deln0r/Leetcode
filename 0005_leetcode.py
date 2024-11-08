5f15cf4d0a834efd55f61d4287981e019c8136c2
Ian Chechin <Delnor40rus@gmail.com>
2024-11-08 21:41:48 +0300

    08.11

--- 0005_leetcode.py
+++ 0005_leetcode.py
@@ -39,3 +39,22 @@ class Solution:
                 else:
                     break
         return ans
+
+class Solution:
+    def longestPalindrome(self, s: str) -> str:
+        ans = ""
+
+        def update_ans(l, r):
+            nonlocal ans
+            while l >= 0 and r < len(s) and s[l] == s[r]:
+                if len(s[l:r+1]) > len(ans):
+                    ans = s[l:r+1]
+                l-=1
+                r+=1
+
+
+        for i in range(len(s)):
+            update_ans(i,i)
+            update_ans(i,i+1)
+
+        return ans
