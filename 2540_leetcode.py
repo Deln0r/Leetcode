2be32d40643778b6814b6b4fe2bf17e46b06ddcb
Ian Chechin <Delnor40rus@gmail.com>
2024-11-09 19:38:25 +0300

    09.11

--- 2540_leetcode.py
+++ 2540_leetcode.py
@@ -8,3 +8,16 @@ class Solution:
                 return num
 
         return(-1)
+
+class Solution:
+    def getCommon(self, nums1: List[int], nums2: List[int]) -> int:
+        l1, l2 = 0, 0
+
+        while l1 < len(nums1) and l2 < len(nums2):
+            if nums1[l1] == nums2[l2]:
+                return nums1[l1]
+            elif nums1[l1] > nums2[l2]:
+                l2 += 1
+            elif nums1[l1] < nums2[l2]:
+                l1 += 1
+        return -1
