de88db175f5587c5272d762deb1308b2477905c6
Ian Chechin <Delnor40rus@gmail.com>
2024-12-18 11:05:11 +0300

    18.12

---
+++ 1475_leetcode.py
@@ -0,0 +1,9 @@
+class Solution:
+    def finalPrices(self, prices: List[int]) -> List[int]:
+        stack = []
+        ans = prices[:]
+        for i , price in enumerate(prices):
+            while stack and prices[stack[-1]] >= price:
+                ans[stack.pop()]-= price
+            stack.append(i)
+        return ans
