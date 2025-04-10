3563470eec476a1440d928f4ee7e28076d070865
Ian Chechin <delnor40rus@gmail.com>
2025-04-10 16:02:05 +0300

    10.04

---
+++ 2999_leetcode.py
@@ -0,0 +1,13 @@
+class Solution:
+    def numberOfPowerfulInt(self, start: int, finish: int, limit: int, s: str) -> int:
+        def at_most(x):
+            str_x = str(x)
+            ans = 0
+            for i in range(len(str_x) - len(s)):
+                ans += min(limit + 1, int(str_x[i])) * (limit + 1) ** (len(str_x) - len(s) - i - 1)
+                if int(str_x[i]) > limit:
+                    break
+            else:
+                ans += (int(str_x[-len(s):]) >= int(s))
+            return ans
+        return at_most(finish) - at_most(start - 1)
