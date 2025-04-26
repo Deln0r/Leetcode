ebc12b0d12818137d6c7f361ac7d8265f81f9b58
Ian Chechin <delnor40rus@gmail.com>
2025-04-26 11:13:38 +0300

    26.04

--- 2444_leetcode.py
+++ 2444_leetcode.py
@@ -11,3 +11,15 @@ class Solution:
                 j2 = i
             ans += max(0, min(j1, j2) - k)
         return ans
+
+class Solution:
+    def countSubarrays(self, nums: List[int], min_k: int, max_k: int) -> int:
+        ans = 0
+        min_i = max_i = i0 = -1
+        for i, x in enumerate(nums):
+            if x == min_k: min_i = i
+            if x == max_k: max_i = i
+            if not min_k <= x <= max_k: i0 = i
+            j = min_i if min_i < max_i else max_i
+            if j > i0: ans += j - i0
+        return ans
