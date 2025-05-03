dbe044a41045b07912f9938a695b95151fa39b75
Ian Chechin <delnor40rus@gmail.com>
2025-05-03 11:26:54 +0300

    03.05

---
+++ 1007_leetcode.py
@@ -0,0 +1,6 @@
+class Solution:
+    def minDominoRotations(self, A, B):
+        for x in [A[0],B[0]]:
+            if all(x in d for d in zip(A, B)):
+                return len(A) - max(A.count(x), B.count(x))
+        return -1
